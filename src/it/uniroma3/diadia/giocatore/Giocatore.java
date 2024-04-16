b64073e90b463237f0805698899d77405a20a10f
dnlsdn <danielesadun@gmail.com>
2024-04-16 11:26:10 +0200

    Aggiunto Commento

--- src/it/uniroma3/diadia/giocatore/Giocatore.java
+++ src/it/uniroma3/diadia/giocatore/Giocatore.java
@@ -1,27 +1,26 @@
 package it.uniroma3.diadia.giocatore;
 
 public class Giocatore {
-	
+
 	static final private int CFU_INIZIALI = 20;
-	
+
 	private Borsa borsa;
 	private int cfu;
 
-	
 	public Giocatore() {
 		this.cfu = CFU_INIZIALI;
-		this.borsa = new Borsa();
+		this.borsa = new Borsa(); // MODIFICATO AAAAAAAA
 	}
-	
+
 	public Borsa getBorsa() {
 		return this.borsa;
 	}
-	
+
 	public int getCfu() {
 		return this.cfu;
 	}
 
 	public void setCfu(int cfu) {
-		this.cfu = cfu;		
+		this.cfu = cfu;
 	}
 }
